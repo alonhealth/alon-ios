89207830eaf912f6cecb9d6de3aabe377b5b4541
baygut <berkaybaygut@gmail.com>
2025-03-16 21:24:30 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "alon-ios",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "alon-ios",
+            targets: ["alon-ios"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "alon-ios"),
+        .testTarget(
+            name: "alon-iosTests",
+            dependencies: ["alon-ios"]
+        ),
+    ]
+)
